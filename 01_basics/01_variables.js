fef0712479673657f50f93157e88145fb2e81be5
Pratham-khot <165020378+Pratham-khot@users.noreply.github.com>
2024-10-07 07:31:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "pratham@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState
+//accountId = 2 //not allowed
+accountEmail = "pk@pk.com"
+accountPassword = "212121"
+accountCity = "Pune"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("") //we are using nodejs not browser
+
+let name = "Pratham"
+let age = 21
+let accloggedin =true
+
+// number => 2 to power 53
+// bigInt
+// String =>"Pratham"
+// boolean =>true/false
+// null =>standalone value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof "Pratham");
+console.log(typeof age);
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
+
