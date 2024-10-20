8a46a3e50dc17002fd1e6e77a2b98ac32ec891ff
Pratham-khot <165020378+Pratham-khot@users.noreply.github.com>
2024-10-20 08:06:40 +0000

    iife done

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,10 @@
 // Immediately Invoked Function Expressions (IIFE)
+
+(function one(){//named iife
+    console.log(`DB connected successfully`);
+    //when we want to write two iife's then we need to give the semicolon after the first iife..
+})();
+((name) =>{
+    console.log(`DB connected to ${name}`);
+    
+})("Pratham")
