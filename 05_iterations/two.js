9c95c6c2c80b55b027542fc6d577dab32730d583
Pratham-khot <165020378+Pratham-khot@users.noreply.github.com>
2024-10-24 08:07:55 +0000

    iterations done

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while loop
+let index=0
+while (index<=10) {
+    //console.log(`Value of index is ${index}`);
+    index=index +2;
+    
+}
+let myArray=["flash","batman","superman"]
+let i=0
+while (i<myArray.length) {
+    //const element=myArray[i];
+    
+    console.log(`Superheros are ${myArray[i]}`);
+    i ++;
+    
+}
+
+// do - while loop
+
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10);
