39a3c83509bec4620a63ee70736019101419916b
Pratham-khot <165020378+Pratham-khot@users.noreply.github.com>
2024-10-25 15:17:50 +0000

    foreach loop done

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -8,10 +8,28 @@ for (const num of arr) {
 }
 const greetings="Hello World!"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    //console.log(`Each char is ${greet}`);
     
 }
 
 // Maps
 
 const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+//console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key, ':-', value);
+    
+}
+
+const myObject ={
+    'game1':'NFS',
+    'game2':'Spiderman'
+}
+// Objects are not iterable therefore below code gives an error..
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value)
+// }
